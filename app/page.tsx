58bd3e1a04a94cddfa92dbb6bdcf7353b8e8852a
Eyuale <eyualekifle99@gmail.com>
2025-02-14 03:59:02 -0900

    some change

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,15 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
